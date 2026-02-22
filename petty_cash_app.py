8c2d66b63b8dd32fae3612b912f7ef8ae6daa338
Achmad Fajar Setiawan <fajarachmad15@gmail.com>
2026-02-22 14:20:56 +0700

    perbaikan 3

--- petty_cash_app.py
+++ petty_cash_app.py
@@ -144,9 +144,9 @@ def run_coa_bot():
     with st.form(key="search_form"):
         col1, col2 = st.columns(2)
         with col1:
-            item_dibeli = st.text_input("Item yg dibeli:", placeholder="Contoh: Aqua Galon, Batagor...")
+            item_dibeli = st.text_input("Item yg dibeli:", placeholder="Contoh: Aqua Galon")
         with col2:
-            digunakan_untuk = st.text_input("Digunakan Untuk:", placeholder="Contoh: Minum karyawan, Jaga malam...")
+            digunakan_untuk = st.text_input("Digunakan Untuk:", placeholder="Contoh: Minum karyawan,")
         
         pph_faktur = st.radio("Potong PPh / Dapat Faktur Pajak?", ("No", "Yes"), index=0, horizontal=True)
         submitted = st.form_submit_button("Cari Rekomendasi COA")
